e51fa2a5dd06c8f0bdffbd515fd407cddce19e74
Daniel Vogler <dvogler98@gmail.com>
2025-03-04 16:13:12 -0800

    fixed database connection bug

--- osp_nce/shared/erm_form.py
+++ osp_nce/shared/erm_form.py
@@ -1,89 +1,75 @@
 import json
 
+import sys
+import os
+
+# Adjust the sys.path to include the directory containing path_utils
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'shared')))
+
+import path_utils
+
+PROJECT_ROOT = path_utils.get_project_root()
+FORM_INPUTS = os.path.join(PROJECT_ROOT, "osp_nce", "shared", "form")
+FIELDS_INPUT = os.path.join(FORM_INPUTS, "fields.json")
+FIELDS_MAP_INPUT = os.path.join(FORM_INPUTS, "fields_map.json")
+FIELDS_MAP_PDF_INPUT = os.path.join(FORM_INPUTS, "fields_map_pdf.json")
 
 class Form:
-
-    def __init__(self):
-        self.fields = {
-            "SFI Current?": "No - Send email to research@uw.edu for review.",
-            "Remaining Balance $$": "Check Award Portal for award balance.",
-            "Is the award in deficit?": "Yes - PI must explain deficit & transfer costs to appropriate non-federal, non-sponsored departmental worktag or provide Sponsor assurance that further funding is forthcoming.",
-            "Is the balance greater than 25% of the total award?": "Yes - PI must provide a programmatic explanation for a large balance.",
-            "Award lines listed or 'extend all' indicated?": "Note in MOD Comments which award lines are to be extended if campus so indicates.",
-            "Temporary Request?": "Include non-sponsored departmental worktag in MOD Comments & History.",
-            "New Cost Share?": "Yes - Attach revised CS Addendum to MOD.",
-            "Human Subjects?": "Yes - Verify and document IRB approval(s). Refer to Human Subjects Review Guidance.",
-            "Animal Use?": "Yes - Verify and document IACUC approval(s). Refer to Animal Use Compliance Verification guidance.",
-            "Prior Approval required?": "Federal award - Review Federal-Wide Research Terms & Conditions (RTCs) Prior Approval Matrix, Appendix A to confirm whether the award requires prior approval.",
-            "Has the project previously been extended? Is this an NIH 2nd+ extension?": "Yes - Ensure that the Budget, Progress Report, and Programmatic Justification are included as 3 separate PDFs.",
-            "Is the request to extend within Sponsor’s required timeframe?": "No - Extension requires Sponsor approval.",
-            "Is this a federal contract?": "Yes - Extension requires Sponsor approval.",
-            "Fixed Price terms?": "No - Extension requires Sponsor approval. Review fixed price terms.",
-            "Paid in full?": "No - Check Award Portal. If outstanding payments exist, deny extension until PI/campus resolve with Sponsor.",
-            "All deliverables submitted?": "No - Extension requires Sponsor approval. Review fixed price terms.",
-            "FAR clause 52.222-54 (e-verify)?": "Yes - Forward E-verify process to your campus contact & state in MOD comments that e-verify is required.",
-            "Review Notes": "Enter any additional notes here.",
-        }
-        self.fields_map = {
-            "ri1": "SFI Current?",
-            "ri2": "Remaining Balance $$",
-            "ri3": "Is the award in deficit?",
-            "ri4": "Is the balance greater than 25% of the total award?",
-            "ri5": "Award lines listed or 'extend all' indicated?",
-            "ri6": "Temporary Request?",
-            "ri7": "New Cost Share?",
-            "ri8": "Human Subjects?",
-            "ri9": "Animal Use?",
-            "ri10": "Prior Approval required?",
-            "ri11": "Has the project previously been extended? Is this an NIH 2nd+ extension?",
-            "ri12": "Is the request to extend within Sponsor’s required timeframe?",
-            "ri13": "Is this a federal contract?",
-            "ri14": "FAR clause 52.222-54 (e-verify)?",
-            "ri15": "Fixed Price terms?",
-            "ri16": "Paid in full?",
-            "ri17": "All deliverables submitted?",
-            "review_notes": "Review Notes",
-        }
-        self.fields_map_pdf = {
-            "pi_name": "PI Name",
-            "mod_id": "MOD/Worktag ID",
-            "ri1": "SFI",
-            "ri2": "RemBal",
-            "ri3": "Deficit?",
-            "ri4": "Greater than 25%?",
-            "ri5": "Award lines",
-            "ri6": "TempReq",
-            "ri7": "CostShare",
-            "ri8": "HumSub",
-            "ri9": "AnimalUse",
-            "ri10": "PriorApp",
-            "ri11": "PrevExt?",
-            "ri12": "ExtendInTime?",
-            "ri13": "FedContract?",
-            "ri14": "FAR clause 5222254",
-            "ri15": "Fixed Price terms",
-            "ri16": "Paid in full",
-            "ri17": "All deliverables  submitted",
-            "review_notes": "Review Notes",
-        }
-
-        # Check if Fields Map keys are a subset of Fields Map PDF keys
+    
+    def __init__(self, input_data = None):
+        
+        if input_data:
+            self.from_dict(input_data)
+            return
+        
+        with open(FIELDS_INPUT) as f:
+            self.fields = json.load(f)
+        
+        with open(FIELDS_MAP_INPUT) as f:
+            self.fields_map = json.load(f)
+            
+        with open(FIELDS_MAP_PDF_INPUT) as f:
+            self.fields_map_pdf = json.load(f)
+            
+        self.answers = {}
+        
+        # Check if Fields Map keys are a subset of Fields Map PDF keys    
         if not self.fields_map.keys() <= self.fields_map_pdf.keys():
             raise ValueError(
-                "There was an error initializing the ERM form. \n"
-                "Fields Map keys are not a subset of Fields Map PDF keys.\n"
-                "To troubleshoot this, check the fields_map.json and fields_map_pdf.json files in the src/shared/form directory."
+            "There was an error initializing the ERM form. \n"
+            "Fields Map keys are not a subset of Fields Map PDF keys.\n"
+            "To troubleshoot this, check the fields_map.json and fields_map_pdf.json files in the src/shared/form directory."
             )
-
+            
     def get_fields(self):
         return self.fields
-
+    
     def get_fields_map(self):
         return self.fields_map
-
+    
     def get_fields_map_pdf(self):
         return self.fields_map_pdf
-
+    
+    def fill_item(self, item_id, answer):
+        
+        self.answers[item_id] = answer
+        
+    def to_dict(self):
+        
+        return {
+            "fields": dict(self.fields),
+            "fields_map": dict(self.fields_map),
+            "fields_map_pdf": dict(self.fields_map_pdf),
+            "answers": dict(self.answers)
+        }
+    
+    def from_dict(self, data):
+        
+        self.fields = data["fields"]
+        self.fields_map = data["fields_map"]
+        self.fields_map_pdf = data["fields_map_pdf"]
+        self.answers = data["answers"]
+            
     def __main__(self):
         "Testing erm_form.py:"
         print("*" * 50)
@@ -94,7 +80,6 @@ class Form:
         print(json.dumps(form.fields_map, indent=4))
         print("Fields Map PDF:")
         print(json.dumps(form.fields_map_pdf, indent=4))
-
 
 if __name__ == "__main__":
     form = Form()
---
+++ src/backend/wsgi.py
@@ -0,0 +1,108 @@
+import sys
+import uvicorn
+import os
+import logging
+
+from osp_nce.backend.libs.sql_connector import SQLConnector
+from osp_nce.backend.libs.sharepoint_connector import SharepointConnector
+from osp_nce.backend.libs.erm_autofiller import ERMAutofiller
+import streamlit as st
+from typing import Dict
+from datetime import datetime
+from fastapi import FastAPI, Depends
+
+logger = logging.getLogger(__name__)
+
+# Get database credentials from environment variables
+db_user = os.getenv("RAD_USER")
+db_password = os.getenv("RAD_PASSWORD")
+db_server = os.getenv("RAD_SERVER")
+db_name = os.getenv("RAD_DATABASE")
+client_id = os.getenv("AZURE_CLIENT_ID")
+tenant_id = os.getenv("AZURE_TENANT_ID")
+
+# Create FastAPI app
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    """Initialize connectors at startup."""
+    app.state.sql_connector = SQLConnector(
+        user=db_user, password=db_password, server=db_server, database=db_name
+    )
+    app.state.sharepoint_connector = SharepointConnector(
+        client_id=client_id, tenant_id=tenant_id
+    )
+    logger.info("Initialized SQLConnector and SharepointConnector.")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    """Cleanup resources at shutdown."""
+    if hasattr(app.state, "sql_connector"):
+        app.state.sql_connector.close()  # Ensure proper cleanup
+    logger.info("Cleaned up resources.")
+
+
+# Dependency to retrieve the singleton instance of SQLConnector
+def get_sql_connector():
+    return app.state.sql_connector
+
+
+# Dependency to retrieve the singleton instance of SharepointConnector
+def get_sharepoint_connector():
+    return app.state.sharepoint_connector
+
+
+@app.get("/ping/")
+async def ping() -> Dict[str, str]:
+    health = True
+    status = 200 if health else 404
+    message = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Status Code {status}, health check {'passed' if health else 'failed'}"
+    return {"message": message}
+
+
+@app.get("/prompt_azure_mfa/")
+async def prompt_azure_mfa(
+    sharepoint_connector: SharepointConnector = Depends(
+        get_sharepoint_connector,
+    )
+) -> Dict[str, str]:
+    try:
+        auth_message = sharepoint_connector.prompt_user()
+        return {"auth_message": auth_message, "Status": "200"}
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+        return {"error": str(e)}
+
+
+@app.get("/acquire_access_token/")
+async def acquire_access_token(
+    sharepoint_connector: SharepointConnector = Depends(get_sharepoint_connector),
+) -> Dict[str, str]:
+    try:
+        sharepoint_connector.acquire_token()
+        return {"Status": "200"}
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+        return {"error": str(e)}
+
+
+@app.get("/run/")
+async def run(
+    mod_id: str,
+    sql_connector: SQLConnector = Depends(get_sql_connector),
+    sharepoint_connector: SharepointConnector = Depends(get_sharepoint_connector),
+) -> Dict[str, str]:
+    try:
+        erm_autofiller = ERMAutofiller(mod_id, sql_connector, sharepoint_connector)
+        erm_autofiller.autofill()
+        return {"Data": erm_autofiller.to_json(), "Status": "200"}
+    except Exception as e:
+        logger.error(f"Error occurred: {str(e)}")
+        return {"error": str(e)}
+
+
+if __name__ == "__main__":
+    uvicorn.run("src.backend.wsgi:app", host="0.0.0.0", port=8000)
