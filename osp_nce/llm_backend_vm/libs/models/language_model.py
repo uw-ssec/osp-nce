797a9094d7b03abcc7f290c110954499aa31e0e8
apoorvasheera <apoorvasheera98@gmail.com>
2025-03-06 02:51:50 +0000

    Refactor LanguageModel class to include quantization parameter and improve model loading logic

--- osp_nce/llm_backend_vm/libs/models/language_model.py
+++ osp_nce/llm_backend_vm/libs/models/language_model.py
@@ -11,35 +11,38 @@ from langchain_huggingface import HuggingFaceEmbeddings
 class LanguageModel():
     def __init__(self, 
                  model_name, 
-                 model_path_prefix = None, 
-                 model_source = None, 
+                 quantization: Literal["8bit", "4bit"] = "8bit",
                  generation_config = {}):
+        
         
         self.model_name = model_name
         self.generation_config = generation_config or {}
-        self.cache_path = os.path.join(os.path.dirname(__file__), "model_cache")
+        self.quantization = quantization
+        
+        self.cache_path = os.path.join(os.path.dirname(__file__), "model_cache", "llm")
         if not os.path.exists(self.cache_path):
             os.makedirs(self.cache_path)
-            os.makedirs(os.path.join(self.cache_path, "llm"))
-        self.model_source = model_source 
+        self.model_path = os.path.join(self.cache_path,  self.model_name.replace("/", "_"))
+        
         self.llm = None
-    def load_language_model(self, 
-                            model_name : str,
-                            quantization: Literal["8bit", "4bit"]
-                            ):
+        self.tokenizer = None
+        self.hg_pipeline = None
+
+    def load_language_model(self):
          # Define the quantization config
-        if quantization == "8bit":
+        if self.quantization == "8bit":
             quantization_config = BitsAndBytesConfig(load_in_8bit=True)
-        if quantization == "4bit":
+        if self.quantization == "4bit":
             quantization_config = BitsAndBytesConfig(load_in_4bit=True)
        
         # Load tokenizer
-        tokenizer = AutoTokenizer.from_pretrained(model_name, cache_dir=os.path.join(self.cache_path, "llm"))
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name, 
+                                                  cache_dir=self.model_path)
 
         # Load model with quantization
         model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            cache_dir=os.path.join(self.cache_path, "llm"),
+            self.model_name,
+            cache_dir=self.model_path,
             device_map="auto",
             quantization_config=quantization_config
         )
@@ -47,10 +50,12 @@ class LanguageModel():
         self.tokenizer = tokenizer
     
     def load_hg_pipeline(self):
+        if self.hg_pipeline:
+            pass
         if self.llm and self.tokenizer:
             pipe = pipeline(
                 "text-generation",
-                model=self.model,
+                model=self.llm,
                 tokenizer=self.tokenizer,
                 max_new_tokens=self.generation_config.get("max_new_tokens", 512),
                 temperature=self.generation_config.get("temperature", 0.8),
@@ -59,14 +64,26 @@ class LanguageModel():
             )
             self.hg_pipeline = HuggingFacePipeline(pipeline=pipe)
         else:
-            logging.info("Model and tokenizer not loaded. Cannot create pipeline.")
-            return None
-    async def inference(self, prompt):
-        if self.hg_pipeline:
-            response = self.hg_pipeline.ainvoke(prompt)
+            logging.info("Model and tokenizer not loaded. Loading now.")
+            self.load_language_model()
+            pipe = pipeline(
+                "text-generation",
+                model=self.llm,
+                tokenizer=self.tokenizer,
+                max_new_tokens=self.generation_config.get("max_new_tokens", 512),
+                temperature=self.generation_config.get("temperature", 0.8),
+                do_sample=self.generation_config.get("do_sample", True),
+                return_full_text=self.generation_config.get("return_full_text", False)
+            )
+            self.hg_pipeline = HuggingFacePipeline(pipeline=pipe)
+        
+    def inference(self, prompt):
+        try:
+            self.load_hg_pipeline()
+            response = self.hg_pipeline.invoke(prompt)
             return response
-        else:
-            logging.info("Model and tokenizer not loaded. Cannot create pipeline.")
+        except Exception as e: 
+            logging.info(f"Inference failed with error {e}")
             return None
             
          
